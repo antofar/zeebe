8105c15ce857bae1e7078bab8742993345700e5f
Christopher Zell <Zelldon91@gmail.com>
2018-04-25 09:30:56 +0200

    clean up

--- broker-core/src/main/java/io/zeebe/broker/clustering/orchestration/TopicCreationReviserService.java
+++ broker-core/src/main/java/io/zeebe/broker/clustering/orchestration/TopicCreationReviserService.java
@@ -2,6 +2,7 @@ package io.zeebe.broker.clustering.orchestration;
 
 import io.zeebe.broker.Loggers;
 import io.zeebe.broker.clustering.base.topology.PartitionInfo;
+import io.zeebe.broker.clustering.base.topology.ReadableTopology;
 import io.zeebe.broker.clustering.base.topology.TopologyManager;
 import io.zeebe.broker.clustering.orchestration.state.ClusterTopicState;
 import io.zeebe.broker.clustering.orchestration.state.TopicInfo;
@@ -49,10 +50,10 @@ public class TopicCreationReviserService extends Actor implements Service<Void>
     @Override
     protected void onActorStarted()
     {
-        actor.runAtFixedRate(TIMER_RATE, this::checkCurrentState);
+        actor.runAtFixedRate(TIMER_RATE, this::topicCreationRevising);
     }
 
-    private void checkCurrentState()
+    private void topicCreationRevising()
     {
         final ActorFuture<Map<DirectBuffer, TopicInfo>> stateFuture = clusterTopicState.getOnlyNotCreatedTopicsFromDesiredState();
 
@@ -60,66 +61,76 @@ public class TopicCreationReviserService extends Actor implements Service<Void>
         {
             if (getDesiredStateError == null)
             {
-                final ActorFuture<Map<DirectBuffer, List<PartitionInfo>>> queryFuture = topologyManager.query(readableTopology ->
-                {
-                    final Map<DirectBuffer, List<PartitionInfo>> currentState = new HashMap<>();
-
-                    final Collection<PartitionInfo> partitions = readableTopology.getPartitions();
-
-                    partitions.forEach(partitionInfo ->
-                        currentState.compute(partitionInfo.getTopicName(),
-                            (s, partitionInfos) ->
-                            {
-                                if (partitionInfos == null)
-                                {
-                                    partitionInfos = new ArrayList<>();
-                                }
-                                partitionInfos.add(partitionInfo);
-                                return partitionInfos;
-                            }));
-
-                    return currentState;
-                });
-
-                actor.runOnCompletion(queryFuture, (currentState, readTopologyError) ->
-                {
-                    if (readTopologyError == null)
-                    {
-                        for (Map.Entry<DirectBuffer, TopicInfo> desiredEntry : desiredState.entrySet())
-                        {
-                            final TopicInfo desiredTopic = desiredEntry.getValue();
-
-                            final List<PartitionInfo> partitionInfos = currentState.get(desiredTopic.getTopicName());
-                            if (partitionInfos == null || partitionInfos.size() < desiredTopic.getPartitionCount())
-                            {
-                                LOG.debug("Send create partition");
-                                // TODO no partitions or not enough for this topic so we create partitions requests
-                                // we need Id generation for that
-                            }
-                            else
-                            {
-
-                               //     partitionInfos.size() == desiredTopic.getPartitionCount()
-                                // TODO write topic CREATED
-                                LOG.debug("Enough partitions created. Current state equals to desired state. Writing Topic {} CREATED.",
-                                    BufferUtil.bufferAsString(desiredTopic.getTopicName()));
-
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Loggers.ORCHESTRATION_LOGGER.error("Error in reading topology.", getDesiredStateError);
-                    }
-                });
+                checkDesiredState(desiredState);
             }
             else
             {
                 Loggers.ORCHESTRATION_LOGGER.error("Error in getting desired state.", getDesiredStateError);
             }
-
         });
+    }
 
+    private void checkDesiredState(Map<DirectBuffer, TopicInfo> desiredState)
+    {
+        final ActorFuture<Map<DirectBuffer, List<PartitionInfo>>> queryFuture = topologyManager.query(this::computeCurrentState);
+
+        actor.runOnCompletion(queryFuture, (currentState, readTopologyError) ->
+        {
+            if (readTopologyError == null)
+            {
+                computeStateDifferences(desiredState, currentState);
+            }
+            else
+            {
+                Loggers.ORCHESTRATION_LOGGER.error("Error in reading topology.", readTopologyError);
+            }
+        });
+    }
+
+    private Map<DirectBuffer, List<PartitionInfo>> computeCurrentState(ReadableTopology readableTopology)
+    {
+        final Map<DirectBuffer, List<PartitionInfo>> currentState = new HashMap<>();
+
+        final Collection<PartitionInfo> partitions = readableTopology.getPartitions();
+
+        partitions.forEach(partitionInfo ->
+            currentState.compute(partitionInfo.getTopicName(),
+                (s, partitionInfos) ->
+                {
+                    if (partitionInfos == null)
+                    {
+                        partitionInfos = new ArrayList<>();
+                    }
+                    partitionInfos.add(partitionInfo);
+                    return partitionInfos;
+                }));
+
+        return currentState;
+    }
+
+    private void computeStateDifferences(Map<DirectBuffer, TopicInfo> desiredState, Map<DirectBuffer, List<PartitionInfo>> currentState)
+    {
+        for (Map.Entry<DirectBuffer, TopicInfo> desiredEntry : desiredState.entrySet())
+        {
+            final TopicInfo desiredTopic = desiredEntry.getValue();
+
+            final List<PartitionInfo> partitionInfos = currentState.get(desiredTopic.getTopicName());
+            if (partitionInfos == null || partitionInfos.size() < desiredTopic.getPartitionCount())
+            {
+                LOG.debug("Send create partition");
+                // TODO no partitions or not enough for this topic so we create partitions requests
+                // we need Id generation for that
+            }
+            else
+            {
+
+               //     partitionInfos.size() == desiredTopic.getPartitionCount()
+                // TODO write topic CREATED
+                LOG.debug("Enough partitions created. Current state equals to desired state. Writing Topic {} CREATED.",
+                    BufferUtil.bufferAsString(desiredTopic.getTopicName()));
+
+            }
+        }
     }
 
     @Override
