e8040d6195118296f162b11c84403e14483f0d19
Sebastian Menski <sebastian.menski@camunda.com>
2018-04-25 15:06:38 +0200

    improve logging on create topic request

--- broker-core/src/main/java/io/zeebe/broker/clustering/orchestration/state/TopicCreateProcessor.java
+++ broker-core/src/main/java/io/zeebe/broker/clustering/orchestration/state/TopicCreateProcessor.java
@@ -1,16 +1,26 @@
 package io.zeebe.broker.clustering.orchestration.state;
 
-import io.zeebe.broker.Loggers;
-import io.zeebe.broker.logstreams.processor.*;
-import io.zeebe.broker.system.log.TopicEvent;
-import io.zeebe.broker.system.log.TopicState;
-import org.agrona.DirectBuffer;
+import static io.zeebe.util.buffer.BufferUtil.bufferAsString;
 
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
+import io.zeebe.broker.Loggers;
+import io.zeebe.broker.logstreams.processor.TypedEvent;
+import io.zeebe.broker.logstreams.processor.TypedEventProcessor;
+import io.zeebe.broker.logstreams.processor.TypedResponseWriter;
+import io.zeebe.broker.logstreams.processor.TypedStreamWriter;
+import io.zeebe.broker.system.log.TopicEvent;
+import io.zeebe.broker.system.log.TopicState;
+import org.agrona.DirectBuffer;
+import org.slf4j.Logger;
+
 public class TopicCreateProcessor implements TypedEventProcessor<TopicEvent>
 {
+
+    private static final Logger LOG = Loggers.ORCHESTRATION_LOGGER;
+
+
     private final Predicate<DirectBuffer> topicExists;
     private final Consumer<TopicInfo> updateTopicState;
 
@@ -21,22 +31,19 @@ public class TopicCreateProcessor implements TypedEventProcessor<TopicEvent>
     }
 
     @Override
-    public void onOpen(TypedStreamProcessor streamProcessor)
-    {
-        Loggers.ORCHESTRATION_LOGGER.debug("staring");
-    }
-
-    @Override
     public void processEvent(final TypedEvent<TopicEvent> event)
     {
         final TopicEvent topicEvent = event.getValue();
 
-        if (topicExists.test(topicEvent.getName()))
+        final DirectBuffer topicName = topicEvent.getName();
+        if (topicExists.test(topicName))
         {
+            LOG.warn("Rejecting topic {} creation as a topic with the same name already exists", bufferAsString(topicName));
             topicEvent.setState(TopicState.CREATE_REJECTED);
         }
         else
         {
+            LOG.info("Creating topic {} with partition count {} and replication factor {}", bufferAsString(topicName), topicEvent.getPartitions(), topicEvent.getReplicationFactor());
             topicEvent.setState(TopicState.CREATING);
         }
     }
